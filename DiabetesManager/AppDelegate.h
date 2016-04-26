9e70c11536491d1893092e0f5859e9adea880596
hr wang <wanghr13@gmail.com>
2016-04-27 06:46:57 +0800

    Initial Commit

---
+++ DiabetesManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DiabetesManager
+//
+//  Created by wangHaoran on 16/4/27.
+//  Copyright © 2016年 Haoran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DiabetesManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DiabetesManager
+//
+//  Created by wangHaoran on 16/4/27.
+//  Copyright © 2016年 Haoran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
